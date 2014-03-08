ad2afe2d130df41abbe6de9bd658f53979d6387d
bredele <olivier.wietrich@gmail.com>
2014-03-08 14:22:17 -0700

    listen changes

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ module.exports = function(ctx) {
 
   var supplant = new Supplant();
 
-	ctx.supplant = function(name) {
-		return supplant.text(name, this.data);
+	ctx.supplant = function(name, fn) {
+		if(!fn) return supplant.text(name, this.data);
+		each(supplant.props(name), function(key, prop) {
+			ctx.on('change ' + prop, function(val) {
+				fn(supplant.text(name, ctx.data));
+			});
+		});
 	};
 
 	ctx.filter = function(name, fn) {
